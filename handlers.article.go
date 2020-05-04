59a677be6cc4f8464a5c8c8ae856fdda934bf28e
Udhay <udhayapsg@gmail.com>
2020-05-05 01:47:59 +0530

    Updated with Json and XML renderings based on the request headers

--- handlers.article.go
+++ handlers.article.go
@@ -9,19 +9,25 @@ import (
 func showIndexPage(context *gin.Context) {
 	articles := getAllArticles()
 
-	context.HTML(http.StatusOK, "index.html", gin.H{
-		"title":   "Home Page",
-		"payload": articles,
-	})
+	render(context,
+			gin.H{
+				"title":   "Home Page",
+				"payload": articles,
+			},
+			"index.html")
 }
 
 func getArticle(context *gin.Context) {
 	if id, err := strconv.Atoi(context.Param("article_id")); err == nil {
 		if article, err := getArticleByID(id); err == nil {
-			context.HTML(http.StatusOK, "article.html", gin.H{
+			render(context, gin.H{
 				"title":   article.Title,
 				"payload": article,
-			})
+			}, "article.html")
+			//context.HTML(http.StatusOK, "article.html", gin.H{
+			//	"title":   article.Title,
+			//	"payload": article,
+			//})
 		} else {
 			context.AbortWithError(http.StatusNotFound, err)
 		}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	_ "net/http"
 )
 
@@ -18,4 +19,20 @@ func main() {
 	initializeRoutes()
 
 	router.Run()
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		// Respond with JSON
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		// Respond with XML
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		// Respond with HTML
+		c.HTML(http.StatusOK, templateName, data)
+	}
+
 }
